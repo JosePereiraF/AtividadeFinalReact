f97c26a4e0befb97e1fde7505c7d53080971bc78
JosePereiraF <jose222125santos@gmail.com>
2024-06-11 01:17:39 -0300

    add forma para trocar o background ainda não ajustado

--- AtividadeFinalReact/RocketStore/src/components/cardItem/CardItem.jsx
+++ AtividadeFinalReact/RocketStore/src/components/cardItem/CardItem.jsx
@@ -16,9 +16,39 @@ export function CardItem({imagem,nome,tipo1,tipo2,quantidadeInicial,input,valor,
         return tipo1 + (resposta ?  `/ ${resposta}` : "");
     };
 
+    function imgBackground(tipo1){
+        //compara o primeiro tipo do pokemon e atribuir o card especifico para ele 
+        const url = "";
+        switch (tipo1) {
+            case "bug":
+                url = "../../assets/img/backgroudPokmon-bug.png"
+                break;
+            case "dark":
+                url =  "../../assets/img/backgroudPokmon-dark.png"
+            default:
+                url = "../../assets/img/backgroudPokmon-dragon.png"
+                break;
+        }
+        const divStyle = {
+            backgroundImage: `url(${url})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+        };
+        return divStyle;
+    }
+/**
+ * 
+
+<CardMedia
+          component="img"
+          height="200"
+          image={image}
+          alt="pokemon"
+        />
+ */
     return(
         <>
-        <div className={styles.card}>
+        <div className={styles.card} style={imgBackground({tipo1})}>
         <div className={styles.divImg}>
         <img src={imagem} alt={nome}className={styles.img}/>
         </div>
