a7f793a4458119f3463ff6980306e81849e81923
robertgall07 <robert.gbg.rg@gmail.com>
2024-06-11 14:07:57 -0300

    Configuração Footer+ troca de sintaxe css == Module + config de FormularioCompativel

--- AtividadeFinalReact/RocketStore/src/pages/Formulario/FormularioCadatros.jsx
+++ AtividadeFinalReact/RocketStore/src/pages/Formulario/FormularioCadatros.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { Container, TextField, Button, Typography } from "@mui/material";
 import { VscEye, VscEyeClosed } from "react-icons/vsc";
+import { NavBarPadrao } from "../../components/NavBar/NavBarP";
+import Footer from "../../components/Footer/footer"; // Importando o Footer corretamente
 
 export function FormPage() {
   const [nome, setNome] = useState("");
@@ -9,7 +11,7 @@ export function FormPage() {
   const [cpf, setCpf] = useState("");
   const [senha, setSenha] = useState("");
   const [confirmarSenha, setConfirmarSenha] = useState("");
-  const [endereco, setEndereco] = useState("");
+  const [Cep, setCep] = useState("");
   const [cadastrado, setCadastrado] = useState(false);
   const [mostrarSenha, setMostrarSenha] = useState(false);
 
@@ -22,7 +24,7 @@ export function FormPage() {
       !cpf ||
       !senha ||
       !confirmarSenha ||
-      !endereco
+      !Cep
     ) {
       alert("Todos os campos são obrigatórios.");
       return;
@@ -47,14 +49,13 @@ export function FormPage() {
       alert("As senhas não coincidem.");
       return;
     }
-    // Se todos os campos passaram nas validações prosseguir com o cadastro
+    // Se todos os campos passaram nas validações, prosseguir com o cadastro
     console.log("Informações do usuário:");
     console.log("Nome:", nome);
     console.log("Telefone:", telefone);
     console.log("Email:", email);
     console.log("CPF:", cpf);
-    console.log("Endereço:", endereco);
-    // handleCadastrar() pode ser chamado aqui
+    console.log("Endereço:", Cep);
     setCadastrado(true); // apenas para demonstração, substitua pela lógica real de cadastro
   };
 
@@ -63,118 +64,126 @@ export function FormPage() {
   };
 
   return (
-    <Container maxWidth="sm" component="article" className="form">
-      <h1>Formulário</h1>
-      <form
-        onSubmit={(event) => {
-          event.preventDefault();
-        }}
-      >
-        <TextField
-          id="nome"
-          label="Nome"
-          variant="outlined"
-          margin="dense"
-          fullWidth
-          value={nome}
-          onChange={(event) =>
-            setNome(event.target.value.replace(/[^a-zA-Z\s]/g, "").trim())
-          }
-        />
-        <TextField
-          id="telefone"
-          label="Telefone"
-          variant="outlined"
-          margin="dense"
-          fullWidth
-          value={telefone}
-          onChange={(event) =>
-            setTelefone(event.target.value.replace(/\D/g, "").substr(0, 10))
-          }
-        />
-        <TextField
-          id="email"
-          label="Email"
-          type="email"
-          variant="outlined"
-          margin="dense"
-          fullWidth
-          value={email}
-          onChange={(event) => setEmail(event.target.value.trim())}
-        />
-        <TextField
-          id="cpf"
-          label="CPF"
-          variant="outlined"
-          margin="dense"
-          fullWidth
-          value={cpf}
-          onChange={(event) =>
-            setCpf(event.target.value.replace(/\D/g, "").substr(0, 11))
-          }
-        />
-        <TextField
-          id="senha"
-          label="Senha"
-          type={mostrarSenha ? "text" : "password"}
-          variant="outlined"
-          margin="dense"
-          fullWidth
-          value={senha}
-          onChange={(event) => setSenha(event.target.value)}
-          InputProps={{
-            endAdornment: (
-              <>
-                {mostrarSenha ? (
-                  <VscEyeClosed
-                    onClick={mostrarOuOcultarSenha}
-                    className="iconeOlho"
-                  />
-                ) : (
-                  <VscEye
-                    onClick={mostrarOuOcultarSenha}
-                    className="iconeOlho"
-                  />
-                )}
-              </>
-            ),
+    <>
+      <div>
+        <NavBarPadrao />
+      </div>
+      <Container maxWidth="sm" component="article" className="form">
+        <h1>Formulário</h1>
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
           }}
-        />
-        <TextField
-          id="confirmarSenha"
-          label="Confirmar Senha"
-          type={mostrarSenha ? "text" : "password"}
-          variant="outlined"
-          margin="dense"
-          fullWidth
-          value={confirmarSenha}
-          onChange={(event) => setConfirmarSenha(event.target.value)}
-        />
-        <TextField
-          id="endereco"
-          label="Endereço"
-          variant="outlined"
-          margin="dense"
-          fullWidth
-          value={endereco}
-          onChange={(event) => setEndereco(event.target.value)}
-        />
-        <Button
-          type="button"
-          className="btn-form"
-          variant="contained"
-          color="primary"
-          onClick={handleCadastrar}
         >
-          Cadastrar
-        </Button>
-      </form>
-      {cadastrado && (
-        <Typography variant="h6" color="secondary" className="mensagem-sucesso">
-          Usuário cadastrado com sucesso!
-        </Typography>
-      )}
-    </Container>
+          <TextField
+            id="nome"
+            label="Nome"
+            variant="outlined"
+            margin="dense"
+            fullWidth
+            value={nome}
+            onChange={(event) =>
+              setNome(event.target.value.replace(/[^a-zA-Z\s]/g, "").trim())
+            }
+          />
+          <TextField
+            id="telefone"
+            label="Telefone"
+            variant="outlined"
+            margin="dense"
+            fullWidth
+            value={telefone}
+            onChange={(event) =>
+              setTelefone(event.target.value.replace(/\D/g, "").substr(0, 10))
+            }
+          />
+          <TextField
+            id="email"
+            label="Email"
+            type="email"
+            variant="outlined"
+            margin="dense"
+            fullWidth
+            value={email}
+            onChange={(event) => setEmail(event.target.value.trim())}
+          />
+          <TextField
+            id="cpf"
+            label="CPF"
+            variant="outlined"
+            margin="dense"
+            fullWidth
+            value={cpf}
+            onChange={(event) =>
+              setCpf(event.target.value.replace(/\D/g, "").substr(0, 11))
+            }
+          />
+          <TextField
+            id="senha"
+            label="Senha"
+            type={mostrarSenha ? "text" : "password"}
+            variant="outlined"
+            margin="dense"
+            fullWidth
+            value={senha}
+            onChange={(event) => setSenha(event.target.value)}
+            InputProps={{
+              endAdornment: (
+                <>
+                  {mostrarSenha ? (
+                    <VscEyeClosed
+                      onClick={mostrarOuOcultarSenha}
+                      className="iconeOlho"
+                    />
+                  ) : (
+                    <VscEye
+                      onClick={mostrarOuOcultarSenha}
+                      className="iconeOlho"
+                    />
+                  )}
+                </>
+              ),
+            }}
+          />
+          <TextField
+            id="confirmarSenha"
+            label="Confirmar Senha"
+            type={mostrarSenha ? "text" : "password"}
+            variant="outlined"
+            margin="dense"
+            fullWidth
+            value={confirmarSenha}
+            onChange={(event) => setConfirmarSenha(event.target.value)}
+          />
+          <TextField
+            id="Cep"
+            label="Cep"
+            variant="outlined"
+            margin="dense"
+            fullWidth
+            value={Cep}
+            onChange={(event) => setCep(event.target.value)}
+          />
+          <Button
+            type="button"
+            className="btn-form"
+            variant="contained"
+            color="primary"
+            onClick={handleCadastrar}
+          >
+            Cadastrar
+          </Button>
+        </form>
+        {cadastrado && (
+          <Typography variant="h6" color="secondary" className="mensagem-sucesso">
+            Usuário cadastrado com sucesso!
+          </Typography>
+        )}
+      </Container>
+      <div>
+        <Footer />
+      </div>
+    </>
   );
 }
 
