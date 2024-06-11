cdcc62efa8e3296c585e3f4002875bc5b0b09aae
lostcake <julianunes3003@gmail.com>
2024-06-11 15:30:31 -0300

    pagina sobre

--- AtividadeFinalReact/RocketStore/src/pages/sobre/sobre.jsx
+++ AtividadeFinalReact/RocketStore/src/pages/sobre/sobre.jsx
@@ -1,15 +1,35 @@
 import React from "react"
 import { NavBarPadrao } from "../../components/NavBar/NavBarP"
-
-
+import Julia from "../../assets/img/Julia.png";
+import Amanda from "../../assets/img/Amanda.png";
+import Guido from "../../assets/img/Guido.png";
+import Jose from "../../assets/img/Jose.png";
+import Robert from "../../assets/img/Robert.png";
+import Rodrigo from "../../assets/img/Rodrigo.png";
+import style from "./style.module.css"
 
 
 export function Sobre(){
 return(
-<><NavBarPadrao/> 
+    /**<h1>SOCORRO D:</h1> */
+<>
+<div>
+    <NavBarPadrao/>
+</div>
 
-<h1>SOCORRO</h1>
 
+<div className={style.caixaPg}>
+    <div className={style.caixa1}>
+    <img src={Julia} className={style.julia} />
+    <img src={Amanda} className={style.amanda} />
+    <img src={Guido} className={style.guido} />
+    </div>
+    <div className={style.caixa2}>
+    <img src={Jose} className={style.jose} />
+    <img src={Robert} className={style.robert} />
+    <img src={Rodrigo} className={style.rodrigo} />
+    </div>
+</div>
 </>
 )
 
