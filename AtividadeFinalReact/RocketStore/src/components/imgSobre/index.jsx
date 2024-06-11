f099c0eda696b846469a16fc83b46e7003807876
Amanda Alves <alvesamanda979@gmail.com>
2024-06-10 21:28:40 -0300

    Tentativa de commit

---
+++ AtividadeFinalReact/RocketStore/src/components/imgSobre/index.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import SobreNos from './pages/sobre/sobre-nos'
+
+
+
+function ComponenteImagens() {
+    return (
+        <>
+            <ComponenteImagens />
+        </>
+    )
+}
+
+export default ComponenteImagens;
---
+++ AtividadeFinalReact/RocketStore/src/pages/sobre/sobre.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Amanda from '.../assets/Amanda.png'
+
+function SobreNos(){
+    <>
+    <div>
+       <ComponenteImagens img = {Amanda} />
+    </div>
+    
+    </>
+   }
+   
+   export default SobreNos
